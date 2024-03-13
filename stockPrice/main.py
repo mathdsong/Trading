9b96d44b00f80b0074b32be09fbc6b177c61c0a2
dongningsong <mathdsong@gmail.com>
2024-03-13 11:58:03 -0600

    define csv_to_close_price function

--- stockPrice/main.py
+++ stockPrice/main.py
@@ -33,3 +33,26 @@ print("7. transpose of open-price pivot table:")
 print(transpose_open_pivot)
 print("8. the mean value for each date: ")
 print(transpose_open_pivot.mean(numeric_only=True))
+
+def csv_to_close_price(csv_filepath, field_names) :
+    """Reads in data from a csv file and produces a DataFrame with close data.
+
+    Parameters
+    ----------
+    csv_filepath : str
+         Thename of the csv file to read
+    field_names : list of str
+        The field names of the field in the csv file
+
+    Returns
+    -------
+    close : DataFrame
+        Close prices for each ticker and date
+    """
+    # read the csv file into the dataframe:
+    df = pd.read_csv(csv_filepath, names=field_names)
+    # generate the close-price pivot table:
+    close_price_pivot_table = df.pivot(index='date', columns='ticker', values='close')
+    return close_price_pivot_table
+
+print(csv_to_close_price(stock_price_path, ['ticker', 'date', 'open', 'high', 'low', 'close', 'volume', 'adj_close', 'adj_volume']))
